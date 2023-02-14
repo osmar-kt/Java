16b9a35ce248791ece891c2a0f39ed9fa6e65a34
Osmar Viorato <79954848+osmvl@users.noreply.github.com>
2023-02-14 17:53:28 -0600

    Create A1FirstSimpleProgram.java

---
+++ Java Reference/P2AnOverviewofJava/A1FirstSimpleProgram.java
@@ -0,0 +1,10 @@
+/*
+  This is a simple Java program.
+  Call this file "A1FirstSimpleProgram.java".
+*/
+class A1FirstSimpleProgram {
+  // Your program begins with a call to main().
+  public static void main(String args[]) {
+  System.out.println("This is a simple Java program.");
+  }
+}
