6921e4e8c5a425b48d9c48d1f91b82b5d133f589
Osmar Viorato <79954848+osmvl@users.noreply.github.com>
2023-02-14 16:53:44 -0600

    Create A2Output.java

---
+++ w3schools/A1JavaTutorial/A2Output.java
@@ -0,0 +1,23 @@
+public class A2Output {
+
+    public static void main(String[] args) {
+        //Output / Print Text
+        //println (Salto de linea)
+        //method to output values or print text in java
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        //The print() method
+        //Which is similar to println()
+        //The only difference is thar it does not insert a new line at the end of the output
+        System.out.print("Hello World! ");
+        System.out.println("I will print on the same line.");
+        //Output / Numbers
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        //You can also perform mathematical calculations inside the println() method:
+        System.out.println(3+3);
+        System.out.println(2*5);
+    }
+}
