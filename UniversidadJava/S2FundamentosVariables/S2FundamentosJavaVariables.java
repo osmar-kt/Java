cff3abbe5214f96d705238cd54f5e725547bd45a
Osmar Viorato <79954848+osmvl@users.noreply.github.com>
2023-02-14 20:58:26 -0600

    Add files via upload

---
+++ UniversidadJava/S2FundamentosVariables/S2FundamentosJavaVariables.java
@@ -0,0 +1,11 @@
+package s2fundamentosjavavariables;
+
+public class S2FundamentosJavaVariables {
+
+    public static void main(String[] args) {
+    
+        System.out.println("Hola Mundo");
+    
+    }
+    
+}
